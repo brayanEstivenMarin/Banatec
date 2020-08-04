365a0239e9667e9e0ace4a5f102290356ba05d0d
Nestor <Nestor@NESTOR-PC>
2020-08-04 16:51:13 -0500

    CAMBIE ID POR CEDULA

--- src/vista/Administradores.java
+++ src/vista/Administradores.java
@@ -87,7 +87,7 @@ public class Administradores extends javax.swing.JInternalFrame {
         });
         jScrollPane1.setViewportView(tabla);
 
-        jLabel2.setText("ID:");
+        jLabel2.setText("CEDULA:");
 
         jLabel3.setText("NOMBRE :");
 
