01ced612144b768398738399e66d48b8f005e863
Nestor <Nestor@NESTOR-PC>
2020-08-04 20:22:51 -0500

    ya puse la base de datos en el servido, no se necesita usar mas xamp o ningun servidor de apache

--- src/controlador/Conexion.java
+++ src/controlador/Conexion.java
@@ -13,7 +13,7 @@ public class Conexion {
         
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/banatec", "root", "");
+            con=DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/sql10358696", "sql10358696", "qHeQvDyW7X");
             
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "error al conectarse con la base de datos");
