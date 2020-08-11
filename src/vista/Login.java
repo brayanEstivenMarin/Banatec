d77a613b5d6cb010e3cfc1151dfb4b0bbcc45884
Nestor <Nestor@NESTOR-PC>
2020-08-11 16:26:57 -0500

    ya puse el login nuevo

--- src/vista/Login.java
+++ src/vista/Login.java
@@ -1,10 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vista;
 
+import com.sun.awt.AWTUtilities;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
 import controlador.Conexion;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -16,13 +15,19 @@ import javax.swing.JOptionPane;
 
 public class Login extends javax.swing.JFrame {
 
-     Conexion conexion= new Conexion();
+  
+    Conexion conexion= new Conexion();
     Connection con;
     PreparedStatement ps;
     ResultSet rs;
-    public Login() {
-        initComponents();
+      public Login() {    
     
+          initComponents();
+        setLocationRelativeTo(null); //Centra la vantana en la pantalla
+        //
+        Shape forma = new RoundRectangle2D.Double(0, 0, getBounds().width, getBounds().height, 20, 20);
+        AWTUtilities.setWindowShape(this, forma);
+       
     }
 
     /**
@@ -34,95 +39,157 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        btniniciar = new javax.swing.JButton();
         txtcorreo = new javax.swing.JTextField();
         pswcontra = new javax.swing.JPasswordField();
-        jLabel2 = new javax.swing.JLabel();
-        btniniciar = new javax.swing.JButton();
-        btncancelar = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+        jSeparator2 = new javax.swing.JSeparator();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jLabel9 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icono2.png"))); // NOI18N
+        jPanel1.setBackground(new java.awt.Color(73, 181, 172));
 
-        jLabel1.setText("CORREO");
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icono.png"))); // NOI18N
 
-        jLabel2.setText("CONTRASEÑA");
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Workstation_100px_1.png"))); // NOI18N
 
-        btniniciar.setText("INICIAR");
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(44, 44, 44)
+                .addComponent(jLabel1)
+                .addContainerGap(47, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(149, 149, 149))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(74, 74, 74)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22))
+        );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 400, 600));
+
+        jPanel2.setBackground(new java.awt.Color(33, 45, 62));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel3.setFont(new java.awt.Font("Gotham Thin", 0, 40)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Iniciar Sesión");
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 70, 275, 53));
+
+        jLabel4.setFont(new java.awt.Font("Gotham Thin", 0, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("CORREO ELECTRÓNICO");
+        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 190, -1, -1));
+
+        jLabel5.setFont(new java.awt.Font("Gotham Thin", 0, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("CONTRASEÑA");
+        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 300, -1, -1));
+
+        btniniciar.setBackground(new java.awt.Color(73, 181, 172));
+        btniniciar.setFont(new java.awt.Font("Gotham Extra Light", 0, 18)); // NOI18N
+        btniniciar.setForeground(new java.awt.Color(255, 255, 255));
+        btniniciar.setText("INGRESAR");
+        btniniciar.setBorderPainted(false);
+        btniniciar.setContentAreaFilled(false);
+        btniniciar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btniniciar.setFocusPainted(false);
+        btniniciar.setFocusable(false);
+        btniniciar.setRequestFocusEnabled(false);
+        btniniciar.setVerifyInputWhenFocusTarget(false);
         btniniciar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btniniciarActionPerformed(evt);
             }
         });
+        jPanel2.add(btniniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 480, 210, 60));
 
-        btncancelar.setText("CANCELAR");
-        btncancelar.addActionListener(new java.awt.event.ActionListener() {
+        txtcorreo.setBackground(new java.awt.Color(33, 45, 62));
+        txtcorreo.setFont(new java.awt.Font("Gotham Thin", 0, 18)); // NOI18N
+        txtcorreo.setForeground(new java.awt.Color(73, 181, 172));
+        txtcorreo.setBorder(null);
+        txtcorreo.setCaretColor(new java.awt.Color(73, 181, 172));
+        jPanel2.add(txtcorreo, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 230, 270, 40));
+
+        pswcontra.setBackground(new java.awt.Color(33, 45, 62));
+        pswcontra.setFont(pswcontra.getFont().deriveFont(pswcontra.getFont().getSize()+7f));
+        pswcontra.setForeground(new java.awt.Color(73, 181, 172));
+        pswcontra.setBorder(null);
+        pswcontra.setCaretColor(new java.awt.Color(73, 181, 172));
+        pswcontra.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btncancelarActionPerformed(evt);
+                pswcontraActionPerformed(evt);
             }
         });
+        jPanel2.add(pswcontra, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 330, 270, 36));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel4)
-                .addGap(389, 389, 389))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(196, 196, 196)
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(167, 167, 167)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txtcorreo, javax.swing.GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
-                            .addComponent(pswcontra)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(191, 191, 191)
-                        .addComponent(btniniciar, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(27, 27, 27)
-                        .addComponent(btncancelar)
-                        .addGap(35, 35, 35)))
-                .addContainerGap(102, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(29, 29, 29)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(txtcorreo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(pswcontra, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btniniciar)
-                    .addComponent(btncancelar))
-                .addGap(47, 47, 47)
-                .addComponent(jLabel4)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+        jSeparator1.setBackground(new java.awt.Color(73, 181, 172));
+        jSeparator1.setForeground(new java.awt.Color(73, 181, 172));
+        jPanel2.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 270, 310, 10));
+
+        jSeparator2.setBackground(new java.awt.Color(73, 181, 172));
+        jSeparator2.setForeground(new java.awt.Color(73, 181, 172));
+        jPanel2.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 370, 310, 10));
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Email_25px.png"))); // NOI18N
+        jPanel2.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 230, 48, 36));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Lock_25px.png"))); // NOI18N
+        jPanel2.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 320, 48, 42));
+
+        jCheckBox1.setBackground(new java.awt.Color(33, 45, 62));
+        jCheckBox1.setFont(new java.awt.Font("Gotham Thin", 0, 14)); // NOI18N
+        jCheckBox1.setForeground(new java.awt.Color(255, 255, 255));
+        jCheckBox1.setText("Recuérdame");
+        jCheckBox1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 400, 160, -1));
+
+        jLabel9.setFont(new java.awt.Font("Gotham Thin", 0, 18)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(73, 181, 172));
+        jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel9.setText("x");
+        jLabel9.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel2.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 0, 30, 30));
+
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 0, 400, 600));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    
+    private void pswcontraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pswcontraActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_pswcontraActionPerformed
+
     private void btniniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btniniciarActionPerformed
-        try {
+         try {
             con = conexion.conectarse();
             ps=con.prepareStatement("Select * from administradores where correo_a=? and contraseña=?");
             ps.setString(1,txtcorreo.getText());
@@ -130,13 +197,13 @@ public class Login extends javax.swing.JFrame {
             
             rs=ps.executeQuery();
             if (rs.next()) {
-                JOptionPane.showMessageDialog(null,"CORRECTO");
+                JOptionPane.showMessageDialog(null,"BIENVENIDO");
                 Inicio inicio=new Inicio();
                 inicio.setVisible(true);
                 this.setVisible(false);
 
             }else{
-                JOptionPane.showMessageDialog(null,"INCORRECTO");
+                JOptionPane.showMessageDialog(null,"USUARIO O CONTRASEÑA INCORRECTA");
                 txtcorreo.setText("");
                 pswcontra.setText("");
             }
@@ -146,10 +213,16 @@ public class Login extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btniniciarActionPerformed
 
-    private void btncancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btncancelarActionPerformed
-        txtcorreo.setText("");
-        pswcontra.setText("");
-    }//GEN-LAST:event_btncancelarActionPerformed
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+                                           
+
+            
 
     /**
      * @param args the command line arguments
@@ -168,32 +241,40 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(vista.Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(vista.Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(vista.Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(vista.Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new vista.Login().setVisible(true);
             }
         });
     }
 
- 
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btncancelar;
     private javax.swing.JButton btniniciar;
+    private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
     private javax.swing.JPasswordField pswcontra;
     private javax.swing.JTextField txtcorreo;
     // End of variables declaration//GEN-END:variables
